0c72b55fdc7f4ec2ca13ce5e901467985ba624b1
nidamunir <nidamunir47@outlook.com>
2024-06-21 08:40:42 +0500

    Embed mouseflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Hotjar from "@hotjar/browser";
 import { useEffect } from "react";
-import Mouseflow from "mouseflow";
+import { mouseflow } from "react-mouseflow";
+import { useRouter } from "next/navigation";
 
 const siteId = 5024784;
 const hotjarVersion = 6;
-
+const mfid = "3fe6bbf0-ff24-458b-a231-57dd2ae2411a";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -17,12 +18,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  useEffect(() => {
+    // Ensure we're on the client side before running Mouseflow code
+    if (typeof window !== "undefined") {
+      // Create a script element
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.mouseflow.com/projects/3fe6bbf0-ff24-458b-a231-57dd2ae2411a.js";
+      script.async = true;
+
+      // Append the script to the document body
+      document.body.appendChild(script);
+
+      // Cleanup function to remove the script when the component unmounts
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, []);
+
   useEffect(() => {
     Hotjar.init(siteId, hotjarVersion);
     console.log("Hotjar initiated, hotjarVersion", hotjarVersion);
+
     Hotjar.event("Page viewed hotjar");
-    Mouseflow.init("2c2b5e25-1639-4105-b4dd-64b73c25ce8d");
-    console.log("mouseflow initiated");
   });
 
   return (
